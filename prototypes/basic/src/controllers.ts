034e5750c30a0fbed2e59ed4ff7caeefdd0ddec2
Ross Jones <ross@register-dynamics.co.uk>
2024-05-22 11:02:37 +0100

    Search impl

--- prototypes/basic/src/controllers.ts
+++ prototypes/basic/src/controllers.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { Config } from "./config";
+import { PARDProduct } from "./models";
 
 export module Controllers {
   const config = new Config();
@@ -9,21 +10,41 @@ export module Controllers {
   }
 
   export function search(request: Request, response: Response) {
+    let term: string = request.query.search?.toString() || "";
     let page = parseInt(request.query.page?.toString() || "1");
     let limit = 10;
     let offset = (page - 1) * limit;
 
     let db = request.app.get("db");
-    let query = `SELECT D.*, O.MAN_ORGANISATION_NAME FROM devices AS D
-     JOIN organisations O on D.MAN_ORGANISATION_ID=O.MAN_ORGANISATION_ID
-     ORDER BY D.DEVICE_TYPE_NAME
-     LIMIT ?
-     OFFSET ?`;
-    let rows = db.prepare(query).all(limit, offset);
 
-    let countQuery = `SELECT COUNT(*) as total FROM devices`;
-    let total = db.prepare(countQuery).get().total;
-    let rangeText = `${offset + 1} - ${offset + rows.length}`;
+    let rangeText = "";
+    let total = 0;
+    let products: PARDProduct[] = [];
+
+    if (term.trim().length == 0) {
+      let [query, countQuery] = browseQuery();
+      products = db.prepare(query).all(limit, offset);
+      total = db.prepare(countQuery).get().total;
+    } else {
+      let query =
+        "select rank, device_id from search where search match ? order by rank LIMIT ? OFFSET ?;";
+      let countQuery =
+        "select count(device_id) as total from search where search match ?";
+      let idResults = db.prepare(query).all(term, limit, offset);
+
+      let ids: number[] = idResults.map((d: any) => {
+        return d.DEVICE_ID;
+      });
+      query = getByIds(ids);
+      console.log(query);
+
+      products = db.prepare(query).all();
+      total = db.prepare(countQuery).get(term).total;
+    }
+
+    if (products.length > 0) {
+      rangeText = `${offset + 1} - ${offset + products.length}`;
+    }
 
     response.render("search", {
       term: request.query["search"] || "",
@@ -31,8 +52,27 @@ export module Controllers {
       total: total,
       page: offset,
       rangeText: rangeText,
-      products: rows,
+      products: products,
     });
+  }
+
+  function browseQuery(): [string, string] {
+    return [
+      `SELECT D.*, O.MAN_ORGANISATION_NAME FROM devices AS D
+     JOIN organisations O on D.MAN_ORGANISATION_ID=O.MAN_ORGANISATION_ID
+     ORDER BY D.DEVICE_TYPE_NAME
+     LIMIT ?
+     OFFSET ?`,
+      `SELECT COUNT(*) as total FROM devices`,
+    ];
+  }
+
+  function getByIds(ids: number[]): string {
+    var inExpression = ids.join(",");
+
+    return `SELECT D.*, O.MAN_ORGANISATION_NAME FROM devices AS D
+     JOIN organisations O on D.MAN_ORGANISATION_ID=O.MAN_ORGANISATION_ID
+     WHERE D.DEVICE_ID IN (${inExpression})`;
   }
 
   export function detail(request: Request, response: Response) {
