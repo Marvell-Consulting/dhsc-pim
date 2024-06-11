4dbdad9f8fa3a503ecf2a0002d288126b927914a
alaric-rd <70887804+alaric-rd@users.noreply.github.com>
2024-06-11 09:48:20 +0100

    GMDN, GUDID and MHRA importers (#21)
    * GMDN and GUDID importers
    
    * .gitignore files to avoid committing sqlite databases by accident
    
    * Additional GUDID files imported
    
    * Import scripts for the MHRA data snapshot we have so far
    
    * README for data importers
    
    * Added comment about the two tables we've ignored thus far
    
    ---------
    
    Co-authored-by: Alaric Snell-Pym <alaric@snell-pym.org.uk>

---
+++ gmdn/schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE gmdn_term (
+       termCode string primary key,
+       termName string,
+       termDefinition string,
+       termStatus string,
+       termIsIVD string,
+       createdDate string,
+       modifiedDate string,
+       obsoletedDate string
+);
