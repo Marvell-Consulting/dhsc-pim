a88ba01ecc22affdae1ce7ab7c0ec84a8420bd41
Ross Jones <ross@register-dynamics.co.uk>
2024-06-14 11:08:59 +0100

    Do not obscure data not available message
    For some boolean fields (actually tri/quad state fields) we previously
    attempted to clean them up. Now we have a pattern for Data not
    available, we can just leave the invalid values as "" to pick up the
    message in the template.

--- prototypes/v2/src/controllers.ts
+++ prototypes/v2/src/controllers.ts
@@ -7,7 +7,6 @@ import { parseFmt, getRenderer, RenderTarget } from "./render";
 const PAGE_SIZE: number = 10;
 
 export module Controllers {
-
   export function index(_request: Request, response: Response) {
     getRenderer(RenderTarget.HTML, "home", response)({});
   }
@@ -211,7 +210,7 @@ export module Controllers {
 
 function clean_boolean(val?: string | undefined): string {
   if (val === null || val === undefined || val == "" || val == "NULL") {
-    return "Not specified";
+    return "";
   }
 
   if (val === "0") {
