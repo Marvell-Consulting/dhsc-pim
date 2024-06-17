7abae4f7f62d129a35fa51e39425b5e395bcfa82
Ross Jones <ross@register-dynamics.co.uk>
2024-06-17 12:05:11 +0100

    Add URLs and controllers for flag/request (#43)
    * Add URLs and controllers for flag/request
    
    `/request/:ID`
    
    Shows all fields with no current value
    
    `/request/:ID/confirmation`
    
    Shows form to obtain email address
    
    `/request/:ID/submitted`
    
    Submission page
    
    `/flag/:ID`
    
    Shows all fields with _some_ data
    
    `/flat/:ID/confirmation`
    
    Shows form to obtain email address
    
    `/flag/:ID/submitted`
    
    Shows submission page
    
    * For form submissions use POST not GET
    
    We should use POST for form submissions to avoid the data appearing in
    the URL address bar in the browser
    
    * Add titles for display
    
    * Add empty PAQ fields to request form

--- prototypes/v2/src/models.ts
+++ prototypes/v2/src/models.ts
@@ -1,3 +1,9 @@
+export class KeyValue {
+  key: string = "";
+  title: string = "";
+  value: any = "";
+}
+
 export class Organisation {
   MAN_ORGANISATION_ID: string = "";
   MAN_ACCOUNT_NUMBER: string = "";
@@ -145,3 +151,110 @@ export class PARDProduct {
   MAXIMUM_NUMBER_REUSE: string = "";
   MAN_ORGANISATION_NAME: string = "";
 }
+
+export const NameMap = new Map<string, string>([
+  ["MANUFACTURER_NAME", "Manufacturer name"],
+  ["GMDN_CODE", "GMDN code"],
+  ["GMDN_TERM_NAME", "GMDN name"],
+  ["BRAND_TRADE_NAME", "Brand name"],
+  ["MODEL", "Model"],
+  ["PRODUCT_CODE", "Manufacturer product code (MPC)"],
+  ["NOTIFIED_BODY_NAME", "Notified Body"],
+  ["IS_STERILE", "Sterile"],
+  ["UDI_NUMBER", "UDI-DI"],
+  ["UNIT_OF_USE_UDI_DI", "Unit of issue"],
+  ["IS_CONTAINING_LATEX", "Contains latex"],
+  ["COMPANY_REGISTRATION_NUMBER", "Companies house registration number"],
+  ["MAN_ADDR_LINE_1", "Manufacturer address"],
+  ["MAN_ADDR_LINE_2", "Manufacturer address (second line)"],
+  ["MAN_ADDR_LINE_3", "Manufacturer address (third line)"],
+  ["MAN_ADDR_LINE_4", "Manufacturer address (fourth line)"],
+  ["MAN_CITY", "Manufacturer address (city)"],
+  ["MAN_STATE", "Manufacturer address (state)"],
+  ["MAN_COUNTYSTATEPROVINCE", "Manufacturer address (county/province)"],
+  ["MAN_POSTCODE", "Manufacturer address (Postcode)"],
+  ["MAN_COUNTRY", "Manufacturer address (Country)"],
+  ["REP_NAME", "Supplier"],
+  ["DEVICE_RISK_SUB_TYPE_DESC", "Risk class"],
+  ["IS_CUSTOM_MADE", "Custom made"],
+  ["IS_STERILE", "Sterile"],
+  ["NOTIFIED_BODY_NAME", "Notified Body"],
+  ["SINGLE_USE_DEVICE", "Single use"],
+  [
+    "IS_PERFORMANCE_STUDIES",
+    "In-Vitro Diagnostic Medical Device for ‘Performance Evaluation’",
+  ],
+  ["DEVICE_IMPLANTABLE", "Implantable"],
+  ["MAXIMUM_NUMBER_REUSE", "Limit of device reprocessing cycles"],
+]);
+
+export const PAQMap = new Map<string, string>([
+  ["P_DESCR", "Product description"],
+  ["P_NHS_ECLASS", "NHS eClass"],
+  ["P_AUTHED_REP", "EU Authorised Representative"],
+  ["P_FIRST_PLACED", "First placed on market"],
+  ["P_STILL_PRODUCED", "Still in production"],
+  ["P_PROD_ENDED", "Production ended"],
+  ["P_MDDC", "Medical devices directive classification"],
+  ["P_AIDD", "Active implantable devices directive"],
+  ["P_IVD", "In-Vitro Diagnostics Medical Device Directive Category"],
+  ["P_MQ_SYSTEMS", "Management / quality system standards"],
+  ["P_CERT_BODY", "Certification body"],
+  ["P_NO_OBJ", "MHRA 'no objection' reference number"],
+  ["P_PMP", "Prescribing medical practitioner"],
+  ["P_MAN_SUPP", "Manufacturer support"],
+  [
+    "P_AVAILABILITY",
+    "Support includes availability of parts and supply of consumables / accessories",
+  ],
+  ["P_INC_TRAINING", "Support includes training, maintenance or repair"],
+  ["P_WARRANTY", "Device warranty period"],
+  ["P_LIFETIME", "Working lifetime"],
+  ["P_NOTIFICATIONS", "Notification of device alerts or upgrades"],
+  ["P_LOAN", "Identical loan equipment available"],
+  ["P_INSTALL", "Installation or ancillary service requirements"],
+  ["P_DISPOSABLE", "Disposable, non-serviceable device"],
+  ["P_MAINT", "Maintenence, repair and technical support provided"],
+  ["P_SERVICE_LOCATIOn", "Servicing facility location"],
+  [
+    "P_STAFF_TRAINED",
+    "Servicing staff verifiably trained and competency assessed",
+  ],
+  ["P_QUAL", "Qualification or competency records available on request"],
+  [
+    "P_CERTIFIED",
+    "Servicing organisation currently certified to any management system standards",
+  ],
+  ["P_MSS", "Management system standards"],
+  [
+    "P_ALT",
+    "Contract alternatives offered include an option for in-house equipment servicing",
+  ],
+  ["P_DECOM", "Level of decontamination needed"],
+  ["P_EN_ISO", "Instructions meet the requirements of EN-ISO-17664"],
+  ["P_PROCESSING", "Needs processing or reprocessing before or between uses"],
+  ["P_UNIQ_ID", "Device is uniquely identifiable"],
+  [
+    "P_TRANSMIT",
+    "Stores or transmits patient information that will require information governance measures",
+  ],
+  ["P_CONNT", "Connects to IT or network services"],
+  ["P_SAFETY_MEASURES", "Requires safety management measures"],
+  ["P_HAZARDS", "Identified hazards"],
+  ["P_PERF_QUALITY", "Requires performance quality assurance measures"],
+  ["P_QA_MEASURES", "Quality assurance measures"],
+  [
+    "P_TRAINING_AVAILE",
+    "Competency-based user training available from the manufacturer or an authorised provider",
+  ],
+  [
+    "P_DECOM_TRAINING",
+    "Competency-based decontamination / reprocessing training available from the manufacturer or an authorised provider",
+  ],
+  [
+    "P_RECORDS",
+    "Qualification / competency records of training providers available upon request",
+  ],
+  ["P_PHONE", "Manufacturer phone number"],
+  ["P_EMAIL", "Manufacturer email"],
+]);
